2d13100e0867de6dc0783f8e8badcb2654ffcce1
thejerremi <240710@edu.p.lodz.pl>
2023-05-05 22:20:08 +0200

    17. Napisano dokumentacje dla klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,14 +1,26 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa dostarczająca wyliczankę służącą do przeprowadzania rozrywek skomplikowanych gier całkowito liczbowych.
+ * Wyróżnia się tym, że nie jest w niej możliwe zgłoszenie (countIn) liczby większej niż bieżąca (peakaboo)
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private static final int ZERO = 0;
     private int totalRejected = ZERO;
-    
 
+
+    /**
+     * @return zwraca aktualną ilość odrzuconych (niepasujących) liczb
+     */
     protected int reportRejected() {
         return totalRejected;
     }
+
+    /**
+     * @param in liczba do dodania
+     * Funkcja dodaje liczbę in do stosu jeśli jest poprawna bądź zwiększa ilość odrzuconych (niepasujących) liczb
+     */
     @Override
     protected void countIn(int in) {
         if (!callCheck() && in > peekaboo())
@@ -17,10 +29,17 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
             super.countIn(in);
     }
 
+    /**
+     * @return zwraca aktualną ilość odrzuconych (niepasujących) liczb
+     */
     public int getTotalRejected() {
         return totalRejected;
     }
 
+    /**
+     * @param totalRejected ilość odrzuconych liczb do ustawienia
+     * Funkcja ustawia ilość odrzuconych (niepasujących) liczb na podaną w parametrze totalRejected
+     */
     public void setTotalRejected(int totalRejected) {
         this.totalRejected = totalRejected;
     }
